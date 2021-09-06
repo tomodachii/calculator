0dadaf3e53293dbb1a7e518dc68fdf40dee538fe
DoTuanHoang <tuanhoang2do2311@gmail.com>
2021-09-06 20:00:58 +0700

    modified keys display

--- dist/script.js
+++ dist/script.js
@@ -1,12 +1,12 @@
-let history = document.getElementById('history-value');
-let output = document.getElementById('output-value');
-let expression = '';
+let history = document.getElementById("history-value");
+let output = document.getElementById("output-value");
+let expression = "";
 let flag = 0;
 
 function clear() {
-  expression = '';
-  setHistory('');
-  setOutput('');
+  expression = "";
+  setHistory("");
+  setOutput("");
   flag = 0;
 }
 
@@ -15,16 +15,16 @@ function setHistory(expression) {
 }
 
 function setOutput(numStr) {
-  if (numStr == '') {
+  if (numStr == "") {
     output.innerHTML = numStr;
   } else {
-    output.innerHTML = Number(numStr).toLocaleString('fi-FI');
+    output.innerHTML = Number(numStr).toLocaleString("fi-FI");
   }
 }
 
 window.onload = () => {
-  document.querySelectorAll('.number').forEach((item) => {
-    item.addEventListener('click', (eventObj) => {
+  document.querySelectorAll(".number").forEach((item) => {
+    item.addEventListener("click", (eventObj) => {
       if (flag) {
         clear();
       }
@@ -33,21 +33,21 @@ window.onload = () => {
     });
   });
 
-  document.querySelectorAll('.operator').forEach((item) => {
-    item.addEventListener('click', (eventObj) => {
+  document.querySelectorAll(".operator").forEach((item) => {
+    item.addEventListener("click", (eventObj) => {
       switch (eventObj.target.id) {
-        case 'clear':
+        case "clear":
           clear();
           break;
-        case 'backspace':
-          setOutput('');
+        case "backspace":
+          setOutput("");
           expression = history.innerHTML.substr(
             0,
             history.innerHTML.length - 1
           );
           setHistory(expression);
           break;
-        case '=':
+        case "=":
           output.innerHTML = eval(expression);
           flag = 1;
           break;
